6187eb00640d5b0c4433e344af0e8f2519f2fc4d
Loïc Angibaud <bald-bearded-man@outlook.com>
2021-04-29 22:02:46 +0200

    Add article with problem (to resolve)

--- ExtractorTest/Program.cs
+++ ExtractorTest/Program.cs
@@ -6,7 +6,7 @@ namespace ExtractorTest
     {
         static void Main(string[] args)
         {
-            String strContent = Article_Content_Extractor.ArticleContentExtractor.GetArticleContentFromURL("http://www.legorafi.fr/2021/04/21/les-connards-annoncent-la-creation-dune-super-league-de-connards/");
+            String strContent = Article_Content_Extractor.ArticleContentExtractor.GetArticleContentFromURL("https://www.lepoint.fr/sante/vaccinodrome-du-stade-de-france-plus-de-limite-d-age-pour-les-metiers-prioritaires-29-04-2021-2424298_40.php");
             Console.WriteLine("Content found :\n" + strContent);
             Console.ReadKey();
         }
